d393974a855da6e7726ecaf45ed7b85f354a7702
Igor E. Novikov <i.novikov@scalr.com>
2019-11-24 21:36:11 +0200

    tests wip

--- src/tests/functional/uc2/cms/test_libcms.py
+++ src/tests/functional/uc2/cms/test_libcms.py
@@ -1,9 +1,7 @@
-import pytest, os
-from PIL import Image
+import os
 
 from uc2 import uc2const
 from uc2.cms import libcms
-
 
 _pkgdir = os.path.abspath(os.path.dirname(__file__))
 
@@ -26,6 +24,80 @@ def test_open_invalid_profile():
     try:
         profile = get_filepath('empty.icm')
         libcms.cms_open_profile_from_file(profile)
+    except libcms.CmsError:
+        return
+    assert False
+
+
+def test_open_absent_profile():
+    try:
+        profile = get_filepath('xxx.icm')
+        libcms.cms_open_profile_from_file(profile)
+    except libcms.CmsError:
+        return
+    assert False
+
+
+def test_create_transform():
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8,
+        OUT_PROFILE, uc2const.TYPE_CMYK_8) is not None
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGBA_8,
+        OUT_PROFILE, uc2const.TYPE_CMYK_8) is not None
+    assert libcms.cms_create_transform(
+        OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        IN_PROFILE, uc2const.TYPE_RGBA_8) is not None
+    assert libcms.cms_create_transform(
+        OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        IN_PROFILE, uc2const.TYPE_RGB_8) is not None
+
+
+def test_create_transform_with_custom_intent():
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8, OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        uc2const.INTENT_PERCEPTUAL) is not None
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8, OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        uc2const.INTENT_RELATIVE_COLORIMETRIC) is not None
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8, OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        uc2const.INTENT_SATURATION) is not None
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8, OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        uc2const.INTENT_ABSOLUTE_COLORIMETRIC) is not None
+
+
+def test_create_transform_with_custom_flags():
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8, OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        uc2const.INTENT_PERCEPTUAL,
+        uc2const.cmsFLAGS_NOTPRECALC | uc2const.cmsFLAGS_GAMUTCHECK) is not None
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8, OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        uc2const.INTENT_PERCEPTUAL,
+        uc2const.cmsFLAGS_PRESERVEBLACK |
+        uc2const.cmsFLAGS_BLACKPOINTCOMPENSATION) is not None
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8, OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        uc2const.INTENT_PERCEPTUAL,
+        uc2const.cmsFLAGS_NOTPRECALC |
+        uc2const.cmsFLAGS_HIGHRESPRECALC) is not None
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8, OUT_PROFILE, uc2const.TYPE_CMYK_8,
+        uc2const.INTENT_PERCEPTUAL,
+        uc2const.cmsFLAGS_NOTPRECALC |
+        uc2const.cmsFLAGS_LOWRESPRECALC) is not None
+
+
+def test_create_transform_with_invalid_intent():
+    assert libcms.cms_create_transform(
+        IN_PROFILE, uc2const.TYPE_RGB_8,
+        OUT_PROFILE, uc2const.TYPE_CMYK_8, 3) is not None
+    try:
+        libcms.cms_create_transform(
+            IN_PROFILE, uc2const.TYPE_RGB_8,
+            OUT_PROFILE, uc2const.TYPE_CMYK_8, 4)
     except libcms.CmsError:
         return
     assert False
