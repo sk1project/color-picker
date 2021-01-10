2027b2933eab94c5a9499ee0913df7c6499f0d5e
sk1project <sk1.project.org@gmail.com>
2021-01-10 02:02:10 +0200

    docker auth

--- bbox.py
+++ bbox.py
@@ -164,6 +164,8 @@ def set_build_stamp():
 
 
 def pull_images():
+    if 'DOCKER_USER' in os.environ:
+        shell('docker login -u %s -p %s'.format(os.environ['DOCKER_USER'], os.environ['DOCKER_PASSWORD']))
     for image in IMAGES:
         msg = 'Pulling %s%s image' % (IMAGE_PREFIX, image)
         msg += ' ' * (50 - len(msg)) + '...'
